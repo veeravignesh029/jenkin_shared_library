2916ccee87f8eec67aee1a64944e00b5233bb69c
veeravignesh029 <168330912+veeravignesh029@users.noreply.github.com>
2025-07-28 14:47:30 +0530

    Update runStages.groovy

--- vars/runStages.groovy
+++ vars/runStages.groovy
@@ -1,2 +1,7 @@
-def call(){
-    sh 'echo Hello World'
+def call(List stagesList) {
+    stagesList.each { stageConfig ->
+        stage(stageConfig.name) {
+            stageConfig.steps.call()
+        }
+    }
+}
